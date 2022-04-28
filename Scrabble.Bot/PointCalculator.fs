dfa86432c56628e883f601eec5b94e65626f0771
amalieboegild <abso@itu.dk>
2022-04-28 19:21:52 +0200

    Start implementing pointCalculator

--- Scrabble.Bot/PointCalculator.fs
+++ Scrabble.Bot/PointCalculator.fs
@@ -2,5 +2,24 @@ namespace DIB
 
 open ScrabbleUtil
 
-module PointCalculator =
-    let calculateWordPoint (word : (coord * (char * int)) list) = failwith "nit"
+
+module internal PointCalculator =
+
+    let private getSquare (coord: coord) (squares: Parser.boardFun2) = 
+        squares coord 
+        |> (fun (StateMonad.Success sq) -> sq) 
+        |> Option.get
+
+    let calculateWordPoint (word : (coord * (char * int)) list) (board: Parser.board) : int = 
+        let mapToList (square:Map<int,Parser.squareFun>) =
+            Map.toList square 
+
+        let appendedList = List.fold (fun acc (l, _) -> (getSquare l board.squares) |> mapToList |> (@) acc  ) List.empty<int*Parser.squareFun> word
+        let sortedList = List.sortByDescending (fun (k, _) -> k) appendedList
+        0
+
+
+    
+    
+
+    
