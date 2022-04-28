eb9a0ba20b4d06bb97a811ae6c32c9efece3bfd6
amalieboegild <abso@itu.dk>
2022-04-28 19:48:58 +0200

    Write todo

--- Scrabble.Bot/PointCalculator.fs
+++ Scrabble.Bot/PointCalculator.fs
@@ -11,13 +11,27 @@ module internal PointCalculator =
         |> Option.get
 
     let calculateWordPoint (word : (coord * (char * int)) list) (board: Parser.board) : int = 
-        let mapToList (square:Map<int,Parser.squareFun>) =
+        let mapToList  (square:Map<int,Parser.squareFun>)  =
             Map.toList square 
 
-        let appendedList = List.fold (fun acc (l, _) -> (getSquare l board.squares) |> mapToList |> (@) acc  ) List.empty<int*Parser.squareFun> word
+        let mapToListFunc (word: (char*int) list) (square:Map<int,Parser.squareFun>)  =
+            Map.toList square
+
+        let letters = List.map (fun (_, letter) -> letter)
+
+        let accFunc acc (l, (c, i)) = (getSquare l board.squares) |> mapToList |> (@) acc 
+
+        let appendedList = List.fold accFunc List.empty<int*Parser.squareFun> word
         let sortedList = List.sortByDescending (fun (k, _) -> k) appendedList
         0
 
+        
+    (*
+        TODO:
+        In order to calculate points we must do the following:
+        - Create an anonymous function that takes in an accumulated result
+        - Create a function(mapToListFunc) that takes the word and the position and creates the anonymous function
+    *)
 
     
     
