7f0d406ee3b3b14f2ad6235992cccbca7bd94861
amalieboegild <abso@itu.dk>
2022-05-04 11:13:39 +0200

    Write test

--- Scrabble.Bot.Tests/PointCalculatorTests.fs
+++ Scrabble.Bot.Tests/PointCalculatorTests.fs
@@ -1,5 +1,21 @@
-module Scrabble.Bot.Tests.PointCalculatorTests
+module Scrabble.Bot.Tests.PointCalculatorTest
 
 open NUnit.Framework
 
-open PointCalculator
+open DIB.PointCalculator
+open ScrabbleUtil
+
+let internal b = Parser.mkBoard (StandardBoard.standardBoard ())
+
+let letterH = ((2,3),('h',3))
+let letterE = ((2,4),('e',1))
+let letterY = ((2,5),('y',7))
+let word = [letterH; letterE; letterY]
+
+
+[<Test>]
+let returns_correct_points () =
+    let points = calculateWordPoint word b
+
+    Assert.AreEqual(11, points)
+
