60b8f79bf5228d58f4ce9f27f28090c2a9b2926a
amalieboegild <abso@itu.dk>
2022-05-04 09:39:59 +0200

    Calculate Points

--- Scrabble.Bot/PointCalculator.fs
+++ Scrabble.Bot/PointCalculator.fs
@@ -13,7 +13,9 @@ module internal PointCalculator =
     let private getResult defvalue = function
         | StateMonad.Success a -> a
         | _ -> defvalue
+
     let calculateWordPoint (word : (coord * (char * int)) list) (board: Parser.board) : int = 
+
         let mapToList  (square:Map<int,Parser.squareFun>)  =
             Map.toList square 
 
@@ -22,19 +24,20 @@ module internal PointCalculator =
 
         let letters = List.map (fun (_, letter) -> letter) word
 
-        let accFunc acc (l, (c, i)) = (getSquare l board.squares) |> mapToList |> (@) acc 
-
-        let appendedList = List.fold accFunc List.empty<int*Parser.squareFun> word
-        let sortedList = List.sortByDescending (fun (k, _) -> k) appendedList
-        0
+        List.mapi (fun index (coord , _) -> mapToListFunc letters index (getSquare coord board.squares)) word
+        |> List.fold (fun acc value -> value @ acc) List.Empty 
+        |> List.sortByDescending (fun (priority , _) -> priority)  
+        |> List.fold (fun acc (_,value) -> value acc |> getResult acc) 0
 
         
-    (*
-        TODO:
-        In order to calculate points we must do the following:
-        - Create an anonymous function that takes in an accumulated result
-        - Create a function(mapToListFunc) that takes the word and the position and creates the anonymous function
-    *)
+        (*
+            TODO: 
+                - Go through each char in the word(coord * (char * int)) with List.mapi and get squareFun to a list
+                - Append the list above to one single list @
+                - Sort the list above
+                - Fold the list to get one int result which is the point scored for that word
+        *)
+        
 
     
     
