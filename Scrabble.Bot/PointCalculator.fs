97ab948fcc369bd9288a2b90ea7fe75201f81de8
amalieboegild <abso@itu.dk>
2022-05-04 10:16:39 +0200

    Add text

--- Scrabble.Bot/PointCalculator.fs
+++ Scrabble.Bot/PointCalculator.fs
@@ -24,19 +24,17 @@ module internal PointCalculator =
 
         let letters = List.map (fun (_, letter) -> letter) word
 
-        List.mapi (fun index (coord , _) -> mapToListFunc letters index (getSquare coord board.squares)) word
-        |> List.fold (fun acc value -> value @ acc) List.Empty 
-        |> List.sortByDescending (fun (priority , _) -> priority)  
-        |> List.fold (fun acc (_,value) -> value acc |> getResult acc) 0
-
-        
-        (*
-            TODO: 
+         (*
+            What is done below: 
                 - Go through each char in the word(coord * (char * int)) with List.mapi and get squareFun to a list
                 - Append the list above to one single list @
                 - Sort the list above
                 - Fold the list to get one int result which is the point scored for that word
         *)
+        List.mapi (fun index (coord , _) -> mapToListFunc letters index (getSquare coord board.squares)) word
+        |> List.fold (fun acc value -> value @ acc) List.Empty 
+        |> List.sortByDescending (fun (priority , _) -> priority)  
+        |> List.fold (fun acc (_,value) -> value acc |> getResult acc) 0
         
 
     
