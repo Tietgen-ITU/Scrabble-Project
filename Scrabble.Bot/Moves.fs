a7c156d5e97f2f8f59532207fee30e04710f29cb
amalieboegild <abso@itu.dk>
2022-05-10 11:26:33 +0200

    Implement getBestMove method

--- Scrabble.Bot/Moves.fs
+++ Scrabble.Bot/Moves.fs
@@ -5,6 +5,7 @@ open ScrabbleUtil
 open ScrabbleUtil.DebugPrint
 open System.Text
 
+
 type Direction =
     | Horizontal
     | Vertical
@@ -18,6 +19,13 @@ type Play =
     | PlayedLetter of Move
 
 type Plays = (Play list * Play list list)
+
+let getBestMove (state: State.state) (currentBest: (Play list * int)) (newPlay: Play list) =
+    let newList = List.map (fun (coord,(_,(letter,point))) -> (coord,(letter,point))) newPlay
+    let newPoints = DIB.PointCalculator.calculateWordPoint newList state.board
+    let getSecondInTuple = (fun (_ , points) -> points)
+    let getFirstInTuple = (fun (letters , _) -> letters)
+    if newPoints > (getSecondInTuple currentBest) then newPlay else getFirstInTuple currentBest
 
 let listToString (chars: (uint32 * (char * int)) list) =
     string (List.fold (fun (sb: StringBuilder) c -> sb.Append(char (c |> snd |> fst))) (new StringBuilder()) chars)
